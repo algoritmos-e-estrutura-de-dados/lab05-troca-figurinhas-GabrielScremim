52250758f2c598ba7a6452661838de054d463cb7
GabrielScremim <gabrielscremim@edu.unifil.br>
2022-10-02 20:01:45 -0300

    entregue

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,9 +1,13 @@
 def maximizar_troca_de_figurinhas(self, figurinhas_da_maria, figurinhas_do_joao):
+    joao = 0
+    maria = 0
 
     if figurinhas_da_maria != figurinhas_da_joao:
-        return figurinhas_da_joao
+        joao = joao +1
+        return joao
     else: figurinhas_da_joao != figurinhas_da_maria
-    return figurinhas_da_maria
+    maria = maria +1
+    return maria
 
 if __name__ == '__main__':
     A, B = input().split(' ')
